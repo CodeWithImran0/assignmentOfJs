4b2fbb38919857242fe2c55a88594d967c95f5a8
Imran chohan <imranhassan03333@gmail.com>
2025-01-28 11:14:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// let userName=prompt("Enter your userName :)")
+// let username="admin";
+// let password="admin123";
+// if(userName==username){
+//     let userPassword=prompt("Enter your password :)")
+//     if(userPassword==password){
+//         console.log("Login successful");
+//     }else{
+//         console.log("Incorrect password");
+
+//     }
+
+
+// }else{
+//     console.log("USERNAME INCORRECT");
+
+// }
+
+
+
+
+
+// let grade = prompt("Enter your grade numbers 0-100 :")
+// let subject1 = "science";
+
+// if (grade >= 0 && grade <= 49) {
+//     console.log("FAIL")
+// }
+// else if (grade >= 50 && grade <= 74) {
+//     console.log("Your grade is C")
+// }
+// else if (grade >= 75 && grade <= 89) {
+//     let subject = prompt("Enter your subject \n Science \n Arts")
+//     if (subject == subject1) {
+//         console.log("Your grade is A")
+//     } else {
+//         console.log("Your grade is B")
+//     }
+// }
+// else if (grade >= 90 && grade <= 100) {
+//     console.log("Your grade is A+")
+// }
+
+
+// let pin=1234;
+// let balance=200000;
+// let userPin=prompt("Enter your ATM pin :)")
+
+// if(userPin==pin){
+//     let options=prompt("Option 1: Withdraw money\nOption 2: Check balance\nOption 3: Exit")
+//     if(options==1){
+//         let withDraw=prompt("Enter your withdraw amount :)")
+//         console.log("your withdraw amont is here",withDraw,"Remaining Balance",balance-withDraw);
+
+//     }else if(options==2){
+//         console.log(balance);
+
+//     }else{
+//         console.log("YOU CHOOSE WRONG OPTIONS SOORY \n TRY AGAIN :)");
+
+//     }
+// }else{
+//     console.log("incorrect pin");
+
+// }
+
+
+// let usermenu = prompt("MENU.IQ\n\n1. Order food\n2. View account\n3. Exit")
+// if(usermenu==1){
+//     let orderFood=prompt("o Sub-option 1: Pizza\no Sub-option 2: Burger")
+// }else if(usermenu==2){
+//     let orderFood=prompt("o Sub-option 1: View balance.\no Sub-option 2: View transactions.")
+// }else{
+//     console.log("wrong option");
+
+// }
+
+// let education = ["SSC.\t", "HSC.\t", "BCS.\t",
+//     "BS.\t", "BCOM.\t", "MS.\t", "M.\t", "Phil.\t", "PhD.\t"]
+// for (const char of education) {
+//     document.write(char)
+// }
+
+
